b0f79f4b99a7264cbfffab16b263c2cd57bcffc5
lucas <lucas@debian.debian>
2025-10-02 21:20:45 -0300

    ajuste relatório

--- sh.c
+++ sh.c
@@ -26,7 +26,7 @@ Fill in the lines below with the name and email of the group members.
 Replace XX with the contribution of each group member in the development of the work.
 
 João Correia Costa, joao120201@ufmg.br, 50%
-Lucas Wiermann Cobo da Silva, lucaswnn@ufmg.br, 50 %
+Lucas Wiermann Cobo da Silva, lucaswnn@ufmg.br, 50%
 
 3. Solutions
 
